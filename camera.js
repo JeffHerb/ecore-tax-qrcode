21cd30316dd83b3e8e88acfff83e308d622b305e
Jeff Herb <herb.jeff@gmail.com>
2022-12-24 18:14:12 -0500

    Adding switch camera control

--- camera.js
+++ camera.js
@@ -44,20 +44,37 @@ class CAMERA extends HTMLElement {
                 pointer-events: auto;
             }
 
-            :host .camera-container.open #cancelCamera {
+            :host .camera-container.open .camera-controls {
+                display:flex;
+                justify-content: center;
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+            }
+
+            :host .camera-container.open .camera-controls #switchCamera,
+            :host .camera-container.open .camera-controls #cancelCamera {
                 position: absolute;
                 background: none;
                 border: 0;
                 cursor: pointer;
                 height: 40px;
+                right: 0;
                 width: 40px;
-                top: 20px;
-                right: 20px;
-                z-index: 11000;
             }
 
-            :host .camera-container.open #cancelCamera:before,
-            :host .camera-container.open #cancelCamera:after {
+            :host .camera-container.open .camera-controls #switchCamera {
+                left: 0;
+            }
+
+            :host .camera-container.open .camera-controls #switchCamera svg {
+                height: 40px;
+                width: 40px;
+            }
+
+            :host .camera-container.open .camera-controls #cancelCamera:before,
+            :host .camera-container.open .camera-controls #cancelCamera:after {
                 position: absolute;
                 left: 15px;
                 content: ' ';
@@ -73,6 +90,13 @@ class CAMERA extends HTMLElement {
                 :host .camera-container.open #cancelCamera:after {
                 transform: rotate(-45deg);
                 }
+
+            :host .camera-container.open .camera-controls #switchCamera {
+                border: 0;
+                cursor: pointer;
+                height: 40px;
+                width: 40px;
+            }
 
             :host .camera-container .flex-container {
                 background: black;
@@ -194,7 +218,12 @@ class CAMERA extends HTMLElement {
 
         return `<div class="camera-container">
             <div class="flex-container">
-                <button id="cancelCamera"></button>
+                <div class="camera-controls">
+                    <button id="switchCamera" title="switch-camera">
+                        <svg width="700pt" height="700pt" viewBox="0 0 700 700" xmlns="http://www.w3.org/2000/svg"><path d="M350 284.45c-32.93 0-59.754 26.824-59.754 59.754 0 32.93 26.824 59.754 59.754 59.754 32.984 0 59.754-26.824 59.754-59.754 0-32.93-26.824-59.754-59.754-59.754zm0 102.7c-23.688 0-42.953-19.266-42.953-42.953 0-23.687 19.266-42.953 42.953-42.953 23.687 0 42.953 19.266 42.953 42.953 0 23.687-19.266 42.953-42.953 42.953z" style="fill:#fff"/><path d="M449.46 250.34h-31.527l-8.176-13.496c-6.328-10.473-17.809-16.969-30.07-16.969h-59.363c-12.207 0-23.688 6.496-30.07 16.969l-8.176 13.496h-31.586c-17.137 0-31.078 13.945-31.078 31.137v125.44c0 17.191 13.945 31.137 31.078 31.137h198.97c17.191 0 31.137-14 31.137-31.137v-125.44c0-17.137-13.945-31.137-31.137-31.137zm14.336 156.57c0 7.894-6.441 14.336-14.336 14.336l-198.97.004c-7.895 0-14.281-6.441-14.281-14.336v-125.44c0-7.894 6.441-14.336 14.281-14.336h41.047l13.047-21.617c3.305-5.488 9.352-8.848 15.68-8.848h59.305c6.383 0 12.375 3.36 15.68 8.848l13.105 21.617h40.992c7.895 0 14.336 6.441 14.336 14.336v125.44zM564.2 378.02c-5.152-6.047-11.031-11.199-17.305-15.566-6.215-4.48-12.77-8.23-19.488-11.535-13.441-6.61-27.441-11.594-41.664-15.512 13.609 5.656 26.77 12.32 38.863 20.328 6.047 3.977 11.816 8.398 17.023 13.215 5.266 4.762 9.91 10.078 13.664 15.793 3.754 5.656 6.441 11.762 7.617 17.977 1.176 6.16.672 12.375-1.23 18.09-3.754 11.535-13.16 21.168-23.97 28.84l-8.96 6.441-21.617-9.574v28.617h64.625l-19.375-8.566c5.77-5.375 11.145-11.367 15.68-18.2 4.594-7 8.344-14.894 9.91-23.52 1.625-8.566 1.121-17.585-1.512-25.647-2.683-8.133-7.11-15.188-12.262-21.18zM158.43 441.58c-5.266-4.762-9.91-10.078-13.664-15.793-3.754-5.656-6.441-11.762-7.617-17.977-1.176-6.16-.672-12.375 1.23-18.09 3.754-11.535 13.16-21.168 23.97-28.84l8.96-6.441 21.617 9.574v-28.617l-64.625.008 19.375 8.566c-5.77 5.375-11.145 11.367-15.68 18.2-4.594 7-8.344 14.894-9.91 23.52-1.684 8.628-1.18 17.59 1.512 25.651 2.633 8.121 7.054 15.176 12.207 21.168 5.152 6.047 11.03 11.2 17.305 15.566 6.215 4.48 12.77 8.23 19.488 11.535 13.44 6.61 27.44 11.594 41.664 15.512-13.61-5.656-26.77-12.32-38.863-20.328-6.051-4.027-11.82-8.398-16.97-13.215z" style="fill:#fff"/></svg>
+                    </button>
+                    <button id="cancelCamera" title="close-camera"></button>
+                </div>
                 <div id="image-wrapper">
                     <video id="video"></video>
                     <canvas id="canvas"></canvas>
